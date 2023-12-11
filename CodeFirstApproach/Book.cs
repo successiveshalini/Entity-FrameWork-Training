e8eda801b318e2311db8be5c30e8ffd6ddadcdf6
successiveshalini <147502984+successiveshalini@users.noreply.github.com>
2023-12-11 18:17:42 +0530

    EntityCodeFirstContext

---
+++ CodeFirstApproach/Book.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirstApproachDataBase1
+{
+    public class Book
+    {
+        public int BookId { get; set; }
+        public string Title { get; set; }
+        //public int AuthorId { get; set; }
+        //public virtual Author Author { get; set; }
+       // public bool IsIssued { get; set; }
+        //public int? IssuedToPersonId { get; set; }
+        //public virtual Person IssuedTo { get; set; }
+    }
+}
---
+++ CodeFirstApproach/EntityCodeFirstContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirstApproachDataBase1
+{
+    public class EntityCodeFirstContext:DbContext
+    {
+        public EntityCodeFirstContext() : base()
+        {
+        }
+        //Adding Domain Classes as DbSet
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Person> Persons { get; set; }
+
+    }
+}
