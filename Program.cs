9093187b2baffac5506444605176944e4e66f4af
successiveshalini <147502984+successiveshalini@users.noreply.github.com>
2023-12-11 18:50:58 +0530

    EntityCodeFirstContext.cs

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirstApproachDataBase1
+{
+    public class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            using (EntityCodeFirstContext context = new EntityCodeFirstContext())
+            { 
+
+                var per = new Person() { FirstName = "Shalini", LastName = "Kumari", PersonId = 101 };
+                context.Persons.Add(per);
+                context.SaveChanges();
+                Console.WriteLine("Person Added");
+                //Console.ReadKey();
+            }
+            //using (EntityCodeFirstContext context = new EntityCodeFirstContext())
+            //{
+
+            //    var book1 = new Book() {BookId = 1, Title = "BookSearch"};
+            //    context.Books.Add(book1);
+            //    //context.SaveChanges();  
+            //    Console.WriteLine("Book Added");
+            //    Console.ReadKey();
+            //}
+            using (EntityCodeFirstContext context = new EntityCodeFirstContext())
+            {
+
+                var auth = new Author() {FirstName = "Soni", LastName = "Kumari", AuthorId = 1 };
+                context.Authors.Add(auth);  
+                context.SaveChanges();
+                Console.WriteLine("Author Added");
+                //Console.ReadKey();
+            }
+
+
+        }
+    }
+}
